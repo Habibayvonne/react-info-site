0291b3ec5c7462bca974c554e86d289b5a32f71d
Habiba <habibayvonne198@gmail.com>
2022-08-30 15:28:16 +0300

    initial commit

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Main() {
+    return <main>
+    <h1 className="header">Fun facts about React</h1>
+    <ul className="facts">
+    <li>Was first released in 2013</li>
+    <li>Was originally created by Jordan walke</li>
+    <li>Has well over 100K stars on Github</li>
+    <li>Is maintained by Facebook</li>
+    <li>s thousands of enterprise apps, including mobile apps</li>
+    </ul>
+    </main>
+}
